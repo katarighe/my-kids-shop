ec6ae13212f3fe9bf547026a3820eb58904ba5b3
katarighe <m.ighe@hotmail.com>
2024-05-13 12:26:35 +0300

    Create the Navbar functions

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../components/assets/logo-transparent.png';
+import carticon from '../components/assets/cart-icon.png';
 
-const Navbar = () => {
-  return (
-    <div className='navbar'>
-      
+const Navbar = () => (
+  <div className="navbar">
+    <div className="nav-logo">
+      <img src={logo} alt="" />
+      <p>THE KIDS SHOP</p>
     </div>
-  )
-}
+    <ul className="nav-menu">
+      <li>Shop</li>
+      <li>Infants</li>
+      <li>Children</li>
+      <li>Accessories</li>
+    </ul>
+    <div className="nav-login-cart">
+      <button type="submit">Login</button>
+      <img src={carticon} alt="" />
+    </div>
+  </div>
+);
 
 export default Navbar;
