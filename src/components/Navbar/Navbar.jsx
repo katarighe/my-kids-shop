57ecfe6944be69e0af65acd481fead3467dd6569
katarighe <m.ighe@hotmail.com>
2024-05-13 13:23:12 +0300

    Update the Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../assets/logo-transparent.png';
 import cartIcon from '../assets/cart_icon.png';
 
-const Navbar = () => (
-  <div className="navbar">
-    <div className="nav-logo">
-      <img src={logo} alt="" />
-      <p>THE KIDS SHOP</p>
+const Navbar = () => {
+  const [menu, setMenu] = useState('shop');
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>THE KIDS SHOP</p>
+      </div>
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu('shop');
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setMenu('shop');
+            }
+          }}
+        >
+          Shop
+          { menu === 'shop' ? <hr /> : null }
+        </li>
+        <li
+          onClick={() => {
+            setMenu('infants');
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setMenu('infants');
+            }
+          }}
+        >
+          Infants
+          { menu === 'infants' ? <hr /> : null }
+        </li>
+        <li
+          onClick={() => {
+            setMenu('children');
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setMenu('children');
+            }
+          }}
+        >
+          Children
+          {menu === 'children' ? <hr /> : null }
+        </li>
+        <li
+          onClick={() => {
+            setMenu('accessories');
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setMenu('accessories');
+            }
+          }}
+        >
+          Accessories
+          { menu === 'accessories' ? <hr /> : null }
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <button type="submit">Login</button>
+        <img src={cartIcon} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
-    <ul className="nav-menu">
-      <li>
-        Shop
-        <hr />
-      </li>
-      <li>Infants</li>
-      <li>Children</li>
-      <li>Accessories</li>
-    </ul>
-    <div className="nav-login-cart">
-      <button type="submit">Login</button>
-      <img src={cartIcon} alt="" />
-      <div className="nav-cart-count">0</div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
