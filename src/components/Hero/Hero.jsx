e6787d620e7476775490c4b5299621f7912bcb75
katarighe <m.ighe@hotmail.com>
2024-05-13 20:18:58 +0300

    Update the changes, to continue tommorrow

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => (
+  <div className="hero">
+    <div className="hero-left">
+      <h1>Welcome to our store</h1>
+      <p>The best place to shop online</p>
+    </div>
+    <div className="hero-right">
+
+    </div>
+  </div>
+);
+
+export default Hero;
