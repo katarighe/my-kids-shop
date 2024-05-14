29bc330c9f5d96ee4143bb1021ee73e784ae0f27
katarighe <m.ighe@hotmail.com>
2024-05-14 11:47:55 +0300

    Update the hero structure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './Hero.css';
+import handIcon from '../assets/hand_icon.png';
+import arrowIcon from '../assets/arrow.png';
+import heroImage from '../assets/hero_image.png';
 
 const Hero = () => (
   <div className="hero">
     <div className="hero-left">
-      <h1>Welcome to our store</h1>
-      <p>The best place to shop online</p>
+      <h1>NEW ARRIVALS ONLY</h1>
+    </div>
+    <div>
+      <div className="hand-hand-icon">
+        <p>New</p>
+        <img src={handIcon} alt="" />
+      </div>
+      <p>collections</p>
+      <p>for everyone</p>
+    </div>
+    <div className="div-hero-latest-button">
+      <div>Latest Collections</div>
+      <img src={arrowIcon} alt="" />
     </div>
     <div className="hero-right">
-
+      <img src={heroImage} alt="" />
     </div>
   </div>
 );
