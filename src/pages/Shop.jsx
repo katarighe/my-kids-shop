6ed895a19fc4ce4a9c810ce5c267c97b1f76250f
katarighe <m.ighe@hotmail.com>
2024-05-14 19:12:46 +0300

    Fix the renaming error

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero/Hero';
 import Popular from '../components/popular/popular';
 import Offers from '../components/offers/offers';
-import NewCollections from '../components/NewCollections/NewCollections';
+import NewCollections from '../components/new-collections/new-collections';
 
 const Shop = () => (
   <div className="shop">
