e59b009c2215a4c5de14352ef41e00be2afb4529
katarighe <m.ighe@hotmail.com>
2024-05-14 14:29:26 +0300

    Add popular component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
+import popular from '../components/popular/popular';
 
 const Shop = () => (
   <div className="shop">
     <Hero />
+    <popular />
   </div>
 );
 
