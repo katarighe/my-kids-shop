d8923b19ed60ca3a4b7f0b1c4c9d0986ef1f357e
katarighe <m.ighe@hotmail.com>
2024-05-14 19:09:16 +0300

    Add NewCollections component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Hero from '../components/Hero/Hero';
 import Popular from '../components/popular/popular';
 import Offers from '../components/offers/offers';
+import NewCollections from '../components/NewCollections/NewCollections';
 
 const Shop = () => (
   <div className="shop">
     <Hero />
     <Popular />
     <Offers />
+    <NewCollections />
   </div>
 );
 
