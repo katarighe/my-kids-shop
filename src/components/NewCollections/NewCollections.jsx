fc8c0226f69dafe4c8b40fe4450c5351ba10999d
katarighe <m.ighe@hotmail.com>
2024-05-14 15:35:30 +0300

    Create the New Collections page

---
+++ src/components/NewCollections/NewCollections.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './NewCollections.css'
+
+const NewCollections = () => (
+   <div className='new-collections'>
+      
+   </div>
+);
+
+export default NewCollections;
