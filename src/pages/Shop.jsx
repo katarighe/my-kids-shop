bf1cf83e6b2ab39013d1e5f9949a2ce8090a9cef
katarighe <m.ighe@hotmail.com>
2024-05-14 14:54:30 +0300

    Add offers component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
 import Popular from '../components/popular/popular';
+import Offers from '../components/offers/offers';
 
 const Shop = () => (
   <div className="shop">
     <Hero />
     <Popular />
+    <Offers />
   </div>
 );
 
