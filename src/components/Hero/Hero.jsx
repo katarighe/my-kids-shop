0420a65fd2729e896fa16e687d35558983192be1
katarighe <m.ighe@hotmail.com>
2024-05-14 12:18:49 +0300

    Update the pages

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,18 +8,18 @@ const Hero = () => (
   <div className="hero">
     <div className="hero-left">
       <h2>NEW ARRIVALS ONLY</h2>
-    </div>
-    <div>
-      <div className="hero-hand-icon">
-        <p>New</p>
-        <img src={handIcon} alt="" />
+      <div>
+        <div className="hero-hand-icon">
+          <p>new kid</p>
+          <img src={handIcon} alt="" />
+        </div>
+        <p>collections</p>
+        <p>for everyone</p>
       </div>
-      <p>collections</p>
-      <p>for everyone</p>
-    </div>
-    <div className="hero-latest-button">
-      <div>Latest Collections</div>
-      <img src={arrowIcon} alt="" />
+      <div className="hero-latest-btn">
+        <div>Latest Collections</div>
+        <img src={arrowIcon} alt="" />
+      </div>
     </div>
     <div className="hero-right">
       <img src={heroImage} alt="" />
