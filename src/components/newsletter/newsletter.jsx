a3ad76c83765da230c988d3038e6a8a7f71cbaae
katarighe <m.ighe@hotmail.com>
2024-05-15 11:24:43 +0300

    Add the page details for the newsletter

--- src/components/newsletter/newsletter.jsx
+++ src/components/newsletter/newsletter.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './newsletter.css';
 
 const newsletter = () => (
-    <div className='newsletter'>
-      
+  <div className="newsletter">
+    <h1>Get Exclusive Offers From Your Inbox</h1>
+    <p>Subscribe to our newsletter today and stay updated!</p>
+    <div>
+      <input type="email" placeholder="Your email here" />
+      <button type="submit">Subscribe</button>
     </div>
+  </div>
 );
 
 export default newsletter;
