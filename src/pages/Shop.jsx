6851faa1133fa57873668cdcfd3618cd1eb95559
katarighe <m.ighe@hotmail.com>
2024-05-15 11:29:30 +0300

    Captialize the letter

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,7 +3,7 @@ import Hero from '../components/Hero/Hero';
 import Popular from '../components/popular/popular';
 import Offers from '../components/offers/offers';
 import NewCollections from '../components/new-collections/new-collections';
-import Newsletter from '../components/newsletter/newsletter';
+import NewsLetter from '../components/newsletter/newsletter';
 
 const Shop = () => (
   <div className="shop">
@@ -11,7 +11,7 @@ const Shop = () => (
     <Popular />
     <Offers />
     <NewCollections />
-    <Newsletter />
+    <NewsLetter />
   </div>
 );
 
