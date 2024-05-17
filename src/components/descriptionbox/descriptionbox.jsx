b33df3161533d367cd4f54549f2f905bf3775c27
katarighe <m.ighe@hotmail.com>
2024-05-17 20:29:57 +0300

    Create DescriptionBox component

--- src/components/descriptionbox/descriptionbox.jsx
+++ src/components/descriptionbox/descriptionbox.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './descriptionbox.css';
 
-const descriptionbox = () => {
-  return (
-    <div>
-
+const descriptionbox = () => (
+  <div className="descriptionbox">
+    <div className="description-box-navigator">
+      <div className="description-nax-box">Description</div>
+      <div className="description-nax-box">Reviews (122)</div>
     </div>
-  );
-};
+    <div className="descriptionbox-description">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus
+        justo, tempus non rutrum id, eleifend sed purus. Aenean urna turpis,
+        laoreet eget metus vel, tincidunt tincidunt velit. Maecenas sit amet
+        faucibus enim, sit amet faucibus orci. Praesent rutrum, sapien vitae
+        tempus mollis, mi tortor porta velit, a malesuada neque magna eu neque.
+        Sed nec nunc a sapien suscipit dapibus non in orci. Nam varius erat
+        nisl, a sollicitudin lacus hendrerit at. Pellentesque commodo arcu a
+        ante malesuada, a ultricies sapien imperdiet.
+      </p>
+    </div>
+  </div>
+);
 
 export default descriptionbox;
