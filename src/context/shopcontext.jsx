505f4dec1c07fb3c59819222c0e3696c8e13e157
katarighe <m.ighe@hotmail.com>
2024-05-17 21:11:33 +0300

    To be updated later

--- src/context/shopcontext.jsx
+++ src/context/shopcontext.jsx
@@ -1,18 +1,34 @@
-import React, { createContext, useMemo } from 'react';
+import React, { createContext, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import allProduct from '../components/assets/all_product';
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+  const cart = {};
+  for (let index = 0; index < allProduct.length+1; index ++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
+
 const ShopContextProvider = (props) => {
-  const contextValue = useMemo(() => ({ allProduct }), []);
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+  const contextValue = useMemo(() => ({ allProduct, cartItems }), [allProduct, cartItems]);
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
 
   const { children } = props;
 
+  const contextValue = {allProduct, cartItems, addToCart, removeFromCart};
   return (
-    <ShopContext.Provider value={contextValue}>
-      {children}
-    </ShopContext.Provider>
+    <ShopContext.Provider value={contextValue}>{children}</ShopContext.Provider>
   );
 };
 
