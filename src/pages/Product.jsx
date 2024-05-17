639b2f79000dbbea4139189f7f5922cf17203455
katarighe <m.ighe@hotmail.com>
2024-05-17 10:31:25 +0300

    Update the component and link breadcrum

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/shopcontext';
+import Breadcrum from '../components/breadcrums/breadcrum';
 
-const Product = () => (
-  <div className="product">
-    <h1>Under Construction</h1>
-  </div>
-);
+const Product = () => {
+  const { allProduct } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = allProduct.find((e) => e.id === Number(productId));
+  return (
+    <div className="breadcrum">
+      <Breadcrum product={product} />
+    </div>
+  );
+};
 
 export default Product;
