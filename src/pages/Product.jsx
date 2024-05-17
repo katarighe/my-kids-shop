af48f41f01bc40fcd86a47148fa0bf0941072cf7
katarighe <m.ighe@hotmail.com>
2024-05-17 11:22:57 +0300

    Fix the tags

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/shopcontext';
 import Breadcrum from '../components/breadcrums/breadcrum';
+import ProductDisplay from '../components/productdisplay/productdisplay';
 
 const Product = () => {
   const { allProduct } = useContext(ShopContext);
@@ -10,6 +11,7 @@ const Product = () => {
   return (
     <div className="breadcrum">
       <Breadcrum product={product} />
+      <ProductDisplay product={product} />
     </div>
   );
 };
