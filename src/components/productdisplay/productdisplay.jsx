7d4e79741096e6dcf52c5f7873d5cfaed6a5a4a9
katarighe <m.ighe@hotmail.com>
2024-05-17 11:22:40 +0300

    Create the product display component

---
+++ src/components/productdisplay/productdisplay.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import './productdisplay.css';
+import starIcon from '../assets/star_icon.png';
+import starDullicon from '../assets/star_dull_icon.png';
+
+const productdisplay = (props) => {
+  const { product } = props;
+  return (
+    <div className="productdisplay">
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
+        <div className="productdisplay-img">
+          <img className="productdisplay-main-img" src={product.image} alt="" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+          <img src={starIcon} alt="" />
+          <img src={starIcon} alt="" />
+          <img src={starIcon} alt="" />
+          <img src={starIcon} alt="" />
+          <img src={starDullicon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">
+            {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-new">
+            {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="productdisplay-right-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-size">
+            <div>XS</div>
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+          </div>
+        </div>
+        <button type="button">Add to Cart</button>
+        <p className="prouctdisplay-right-category">
+          <span>Category: </span>
+          Your Category Here
+        </p>
+        <p className="prouctdisplay-right-tags">
+          <span>Tags: </span>
+          Your Tags Here
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default productdisplay;
