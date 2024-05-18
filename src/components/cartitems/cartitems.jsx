114919ef609b0470c4650f51e614e38c16608d52
katarighe <m.ighe@hotmail.com>
2024-05-18 15:05:06 +0300

    Update the functions

--- src/components/cartitems/cartitems.jsx
+++ src/components/cartitems/cartitems.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
+import { ShopContext } from '../../context/shopcontext';
 import './cartitems.css';
+import removeIcon from '../assets/cart_cross_icon.png';
 
-const cartitems = () => {
+const CartItems = () => {
   const { allProduct, cartItems, removeFromCart } = useContext(ShopContext);
   return (
     <div className="cart-items">
@@ -13,8 +15,36 @@ const cartitems = () => {
         <p>Total</p>
         <p>Remove</p>
       </div>
+      <hr />
+      {allProduct.map((e) => {
+        if (cartItems[e.id] > 0) {
+          return (
+            <div key={e.id}>
+              <div className="cartitems-format">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.name}</p>
+                <p>{e.new_price}</p>
+                <button type="button" className="cartitems-quantity">
+                  {cartItems[e.id]}
+                </button>
+                <p>{e.new_price * cartItems[e.id]}</p>
+                {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+                <img
+                  src={removeIcon}
+                  onClick={() => removeFromCart(e.id)}
+                  onKeyDown={() => removeFromCart(e.id)}
+                  alt=""
+                />
+                {/* eslint-enable jsx-a11y/no-noninteractive-element-interactions */}
+              </div>
+              <hr />
+            </div>
+          );
+        }
+        return null;
+      })}
     </div>
   );
 };
 
-export default cartitems;
+export default CartItems;
